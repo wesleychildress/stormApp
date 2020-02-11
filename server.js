bbb2307ddeb4d5416ccb9631929ae2f1753a58b2
WesleyChildress <wesleychildress@icloud.com>
2020-02-11 00:08:22 -0500

    server.js edits

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/safe-everglades-20368'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/safe-everglades-20368/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
