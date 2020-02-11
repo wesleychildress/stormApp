cbbc7f1f5b62c4d36543df46379e6a3a1f1d5cb4
WesleyChildress <wesleychildress@icloud.com>
2020-02-10 23:12:17 -0500

    server.js edits

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-﻿//Install express server
+﻿//* Install express server */
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+//* Serve only the static files form the dist directory */
 app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function (req, res) {
@@ -12,5 +12,5 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+//* Start the app by listening on the default Heroku port */
 app.listen(process.env.PORT || 8080);
